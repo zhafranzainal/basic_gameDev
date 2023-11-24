d23de9ecc002ff84752cd0aff4357aabf6da1b8e
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-25 05:02:10 +0800

    Create person.js

---
+++ Meeting 1 - How Cool is Programming World/person.js
@@ -0,0 +1,12 @@
+var person = {
+    name: 'John',
+    age: 15,
+    gender: 'male',
+    interest: 'coding'
+};
+
+// Accessing property using dot notation
+console.log(person.age);
+
+// Accessing property using bracket notation (with the property name as a string)
+console.log(person['age']);
