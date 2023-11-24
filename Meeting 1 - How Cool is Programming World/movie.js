62af81a716563a5d817b6cd95265a5df7aaf17d2
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-25 05:32:39 +0800

    Create movie.js

---
+++ Meeting 1 - How Cool is Programming World/movie.js
@@ -0,0 +1,8 @@
+var movies = {
+    title: 'Star Wars: The Rise of Skywalker',
+    releaseDate: 'December 16, 2019',
+    genre: 'Science fiction'
+};
+
+console.log(movies.title);
+console.log(movies['title']);
