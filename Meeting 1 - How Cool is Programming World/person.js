f9cce4f71d0166ce5544c417f682840758d8787a
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-25 05:17:16 +0800

    Update person.js

--- Meeting 1 - How Cool is Programming World/person.js
+++ Meeting 1 - How Cool is Programming World/person.js
@@ -2,7 +2,11 @@ var person = {
     name: 'John',
     age: 15,
     gender: 'male',
-    interest: 'coding'
+    interest: 'coding',
+
+    greeting: function () {
+        console.log("Hi, I'm " + person.name);
+    }
 };
 
 // Accessing property using dot notation
@@ -10,3 +14,6 @@ console.log(person.age);
 
 // Accessing property using bracket notation (with the property name as a string)
 console.log(person['age']);
+
+// Calling object's method/function/behaviour
+person.greeting();
