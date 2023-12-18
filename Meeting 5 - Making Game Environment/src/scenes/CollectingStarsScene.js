3baed49e11fdb7a4c19214667996343f6fa9476b
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-18 20:00:04 +0800

    Load sprite sheet

--- Meeting 5 - Making Game Environment/src/scenes/CollectingStarsScene.js
+++ Meeting 5 - Making Game Environment/src/scenes/CollectingStarsScene.js
@@ -7,10 +7,17 @@ export default class CollectingStarsScene extends Phaser.Scene {
 	}
 
 	preload() {
+
 		this.load.image('platform', 'images/platform.png');
 		this.load.image('star', 'images/star.png');
 		this.load.image('sky', 'images/sky.png');
 		this.load.image('bomb', 'images/bomb.png');
+
+		this.load.spritesheet('dude', 'images/dude.png', {
+			frameWidth: 32,
+			frameHeight: 48
+		});
+
 	}
 
 	create() {
