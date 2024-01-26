ac60684b4bdb6270a7743217048a69b2be9cbdc5
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-27 03:18:05 +0800

    Add 3 buttons

--- Meeting 7 - Corona Buster/src/scenes/CoronaBusterScene.js
+++ Meeting 7 - Corona Buster/src/scenes/CoronaBusterScene.js
@@ -7,12 +7,24 @@ export default class CoronaBusterScene extends Phaser.Scene {
     }
 
     init() {
+
         this.clouds = undefined;
+
+        this.nav_left = false;
+        this.nav_right = false;
+        this.shoot = false;
+
     }
 
     preload() {
+
         this.load.image('background', 'images/bg-layer1.png');
         this.load.image('cloud', 'images/cloud.png');
+
+        this.load.image('left-btn', 'images/left-btn.png');
+        this.load.image('right-btn', 'images/right-btn.png');
+        this.load.image('shoot-btn', 'images/shoot-btn.png');
+
     }
 
     create() {
@@ -25,12 +37,14 @@ export default class CoronaBusterScene extends Phaser.Scene {
         this.clouds = this.physics.add.group({
             key: 'cloud',
             repeat: 10,
-        })
+        });
 
         Phaser.Actions.RandomRectangle(
             this.clouds.getChildren(),
             this.physics.world.bounds
-        )
+        );
+
+        this.createButton();
 
     }
 
@@ -44,12 +58,54 @@ export default class CoronaBusterScene extends Phaser.Scene {
             // @ts-ignore
             if (child.y > this.scale.height) {
                 // @ts-ignore
-                child.x = Phaser.Math.Between(10, 400)
+                child.x = Phaser.Math.Between(10, 400);
                 // @ts-ignore
                 child.y = 0;
             }
 
         })
+
+    }
+
+    createButton() {
+
+        this.input.addPointer(3);
+
+        let nav_left = this.add.image(
+            50, 550, 'left-btn'
+        ).setInteractive().setDepth(0.5).setAlpha(0.8);
+
+        let nav_right = this.add.image(
+            nav_left.x + nav_left.displayWidth + 20, 550, 'right-btn'
+        ).setInteractive().setDepth(0.5).setAlpha(0.8);
+
+        let shoot = this.add.image(
+            320, 550, 'shoot-btn'
+        ).setInteractive().setDepth(0.5).setAlpha(0.8);
+
+        nav_left.on('pointerdown', () => {
+            this.nav_left = true
+        }, this);
+
+        nav_left.on('pointerout', () => {
+            this.nav_left = false
+        }, this);
+
+        nav_right.on('pointerdown', () => {
+            this.nav_right = true
+        }, this)
+
+        nav_right.on('pointerout', () => {
+            this.nav_right = false
+        }, this)
+
+        shoot.on('pointerdown', () => {
+            this.shoot = true
+        }, this)
+
+        shoot.on('pointerout', () => {
+            this.shoot = false
+        }, this)
 
     }
 
