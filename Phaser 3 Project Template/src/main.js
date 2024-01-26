bf656c3969236bcc4f1ab1f5cd19073a28584106
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-27 01:08:20 +0800

    Duplicate new project template

---
+++ Phaser 3 Project Template/src/main.js
@@ -0,0 +1,18 @@
+import Phaser from 'phaser';
+import HelloWorldScene from './scenes/HelloWorldScene';
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'app',
+    width: 1920,
+    height: 1080,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 },
+        },
+    },
+    scene: [HelloWorldScene],
+}
+
+export default new Phaser.Game(config)
