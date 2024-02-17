fda58984688570d0d60b8240cad52d646112013d
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-17 21:27:38 +0800

    Create Corona Buster template

---
+++ Meeting 11 - Score and Life/src/ui/Laser.js
@@ -0,0 +1,30 @@
+import Phaser from 'phaser';
+
+export default class Laser extends Phaser.Physics.Arcade.Sprite {
+
+    constructor(scene, x, y, texture) {
+        super(scene, x, y, texture);
+        this.setScale(2);
+    }
+
+    fire(x, y) {
+        this.setPosition(x, y - 50);
+        this.setActive(true);
+        this.setVisible(true);
+    }
+
+    die() {
+        this.destroy();
+    }
+
+    update(time) {
+
+        this.setVelocityY(-200);
+
+        if (this.y < -10) {
+            this.die();
+        }
+
+    }
+
+}
