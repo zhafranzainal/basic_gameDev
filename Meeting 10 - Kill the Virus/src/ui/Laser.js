840f6d3ebfb1ceffd2374ce4f216d55f64aedc84
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-17 17:22:01 +0800

    Create Laser.js

---
+++ Meeting 10 - Kill the Virus/src/ui/Laser.js
@@ -0,0 +1,28 @@
+import Phaser from 'phaser';
+
+export default class Laser extends Phaser.Physics.Arcade.Sprite {
+
+    constructor(scene, x, y, texture) {
+        super(scene, x, y, texture);
+        this.setScale(2);
+    }
+
+    fire(x, y) {
+        this.setPosition(x, y - 50);
+        this.setActive(true);
+        this.setVisible(true);
+    }
+
+    die() {
+        this.destroy();
+    }
+
+    update(time) {
+
+        if (this.y < -10) {
+            this.die();
+        }
+
+    }
+
+}
