ccfc11219346ab106351efe74870cf48dce6234e
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-20 04:15:43 +0800

    Create Corona Buster template

---
+++ Meeting 12 - Increase Life and Add Sound/src/scenes/GameOverScene.js
@@ -0,0 +1,38 @@
+import Phaser from 'phaser';
+
+export default class GameOverScene extends Phaser.Scene {
+
+    constructor() {
+        super('over-scene');
+    }
+
+    init(data) {
+        this.replayButton = undefined;
+        this.score = data.score;
+    }
+
+    preload() {
+        this.load.image('background', 'images/bg-layer1.png');
+        this.load.image('gameover', 'images/gameover.png');
+        this.load.image('replay-button', 'images/replay.png');
+    }
+
+    create() {
+        this.add.image(200, 320, 'background');
+        this.add.image(200, 200, 'gameover');
+        this.add.text(100, 300, 'Score: ' + this.score, {
+            fontSize: '32px',
+            // @ts-ignore
+            fill: 'black'
+        });
+
+        this.replayButton = this.add.image(200, 400, 'replay-button')
+            .setInteractive().setScale(0.5);
+
+        this.replayButton.once('pointerup', () => {
+            this.scene.start('corona-buster-scene');
+        }, this);
+
+    }
+
+}
