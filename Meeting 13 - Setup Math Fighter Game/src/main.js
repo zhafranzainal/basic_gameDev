0f277cc524f40b02d117fdec6d8594e7c77b3324
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-28 17:13:37 +0800

    Configure game layout size

--- Meeting 13 - Setup Math Fighter Game/src/main.js
+++ Meeting 13 - Setup Math Fighter Game/src/main.js
@@ -4,13 +4,17 @@ import MathFighterScene from './scenes/MathFIghterScene';
 const config = {
     type: Phaser.AUTO,
     parent: 'app',
-    width: 1920,
-    height: 1080,
+    width: 480,
+    height: 640,
     physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 200 },
         },
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
     },
     scene: [MathFighterScene],
 }
