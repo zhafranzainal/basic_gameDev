060c4d46c980ac7c8e18c1e246069718503a6c5f
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-28 23:43:45 +0800

    Add background

--- Meeting 13 - Setup Math Fighter Game/src/scenes/MathFighterScene.js
+++ Meeting 13 - Setup Math Fighter Game/src/scenes/MathFighterScene.js
@@ -6,12 +6,48 @@ export default class MathFighterScene extends Phaser.Scene {
         super('math-fighter-scene');
     }
 
+    init() {
+    }
+
     preload() {
-        this.load.image('', 'images/');
+
+        this.load.image('background', 'images/bg_layer1.png');
+        this.load.image('fight-bg', 'images/fight-bg.png');
+        this.load.image('tile', 'images/tile.png');
+        this.load.image('start-btn', 'images/start_button.png');
+
+        this.load.spritesheet('player', 'images/warrior1.png', {
+            frameWidth: 80,
+            frameHeight: 80
+        });
+
+        this.load.spritesheet('enemy', 'images/warrior2.png', {
+            frameWidth: 80,
+            frameHeight: 80
+        });
+
+        this.load.spritesheet('numbers', 'images/numbers.png', {
+            frameWidth: 131,
+            frameHeight: 71.25
+        });
+
+        this.load.spritesheet('slash', 'images/slash.png', {
+            frameWidth: 42,
+            frameHeight: 88
+        });
+
     }
 
     create() {
-        this.add.image(960, 540, '');
+
+        this.add.image(240, 320, 'background');
+
+        const fight_bg = this.add.image(240, 160, 'fight-bg');
+        const tile = this.physics.add.staticImage(240, fight_bg.height - 40, 'tile');
+
+    }
+
+    update() {
     }
 
 }
