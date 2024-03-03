a092ab25dcdc4b8224f13ef7b80bbc07a6ebc216
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-03-03 21:48:54 +0800

    Add game over scene

--- Meeting 17 - Score and Timer/src/main.js
+++ Meeting 17 - Score and Timer/src/main.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import MathFighterScene from './scenes/MathFighterScene';
+import GameOverScene from './scenes/GameOverScene';
 
 const config = {
     type: Phaser.AUTO,
@@ -17,7 +18,7 @@ const config = {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
-    scene: [MathFighterScene],
+    scene: [MathFighterScene, GameOverScene],
 }
 
 export default new Phaser.Game(config);
