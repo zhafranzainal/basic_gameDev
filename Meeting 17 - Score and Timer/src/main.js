6b3c173c4c4c00557361b1ba6505d20540aea6fc
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-03-03 20:59:06 +0800

    Create Math Fighter template

---
+++ Meeting 17 - Score and Timer/src/main.js
@@ -0,0 +1,23 @@
+import Phaser from 'phaser';
+import MathFighterScene from './scenes/MathFighterScene';
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'app',
+    width: 480,
+    height: 640,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 },
+            debug: true
+        },
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [MathFighterScene],
+}
+
+export default new Phaser.Game(config);
